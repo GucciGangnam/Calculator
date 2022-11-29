c95a732e8ab39cb9c1412d0561de65ed28e1914c
Gucci Gangnam <diviner02hive@icloud.com>
2022-11-29 11:34:12 +0700

    lots of updates.  Notes added. Calculator complete

--- main.js
+++ main.js
@@ -1,27 +1,100 @@
 
 
+var displayValue = ""
+var previousNum = ''
+var operator = ''
+let equation = (previousNum, operator, displayValue )
 
-function add () { 
+// clear function
 
+document.getElementById("clearbtn").onclick = function () {clear()};
+function clear() { 
+    answer.innerHTML = ''
+    previous.innerHTML = ''
+    displayValue = ''
+    previousNum = ''
+    operator = ''
+    console.log('youcleared')
 }
 
-function subtract () { 
 
-}
+// display numbers on screen and add to display value
+const screen = document.getElementById("answer")
+const allNumberButtons = document.getElementsByClassName('buttonNum');
 
-function times () { 
+Array.prototype.forEach.call(allNumberButtons, (e) => e.addEventListener('click', () => answer.innerHTML += e.innerHTML));
+Array.prototype.forEach.call(allNumberButtons, (e) => e.addEventListener('click', () => displayValue += e.innerHTML));
 
-}
 
-function devide () { 
+// when operator button is is clicked: 
+    //store id=answer as previous number
+    //
 
-}
+const allFunButtons = document.getElementsByClassName('buttonFun');
 
-function equal () { 
+Array.prototype.forEach.call(allFunButtons, (e) => e.addEventListener('click', () => {  
+    if (displayValue == '') {
+        return
+    } else 
+    previousNum = displayValue
+    previous.innerHTML = previousNum;
+    displayValue = ''
+    answer.innerHTML = ''
+    operator = e.innerHTML  //is there another way to feth the operator so i can use the real math signs in the html??
+}))
 
-}
+    
 
-function clear () { 
+
+    /* document.getElementById("button/").onclick = function() {
+        if (displayValue == '') {
+            return
+        } else 
+        previousNum = displayValue
+        previous.innerHTML = previousNum;
+        displayValue = ''
+        answer.innerHTML = ''
+        operator = "/"
+        
+        console.log('prev' + previousNum + 'disp' + displayValue + 'oper' + operator)
+    };
+*/
+
+
+//point button 
+document.getElementById("button.").onclick = function() { 
+    if ((displayValue.includes('.')) || (displayValue == '')) { return }
+    else 
+    {displayValue += '.';
+    answer.innerHTML += '.'; 
+}}
+
+
+
+
+
+
+
+
+ //operator function
+ document.getElementById("button=").onclick = function() { 
+     if (operator == '/') {
+        displayValue = (previousNum / displayValue);
+        answer.innerHTML = displayValue;
+
+    }
+    else if (operator == '*') { 
+        displayValue = (previousNum * displayValue);
+        answer.innerHTML = displayValue;
+    }
+    else if (operator == '+') { 
+        displayValue = parseFloat(previousNum) + parseFloat(displayValue);
+        answer.innerHTML = displayValue;
+    }
+    else if (operator == '-') { 
+        displayValue = parseFloat(previousNum) - parseFloat(displayValue);
+        answer.innerHTML = displayValue;
+    }
 
 }
 
